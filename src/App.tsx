c042ead9553223e1f36b8e6ea5ff6d578d990984
Adam Vician <adamvician@gmail.com>
2022-05-06 14:54:42 +0200

    Added stepper

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,28 @@ import React from "react";
 import styled from "styled-components";
 import { Footer } from "./components/footer";
 import { Header } from "./components/header";
+import { Stepper } from "./components/stepper";
+import { Wrapper } from "./components/wrapper";
 
 const Main = styled.main`
-	height: 300px;
+	padding: 64px;
+	display: flex;
 `;
 
 const App: React.FC = () => {
 	return (
 		<div>
 			<Header />
-			<Main></Main>
+			<Wrapper>
+				<Main>
+					<div style={{ flex: 1 }}>
+						<Stepper stepCount={3} activeStep={0} />
+						<h1>Vyberte si možnosť, ako chcete pomôcť</h1>
+					</div>
+					<img src="/dogmask.png" alt="Dog drinking water" />
+				</Main>
+			</Wrapper>
+
 			<Footer></Footer>
 		</div>
 	);
---
+++ src/components/stepper/index.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import styled, { css } from "styled-components";
+
+interface Props {
+	activeStep: number;
+	stepCount: number;
+}
+
+interface StepItemProps {
+	active: boolean;
+}
+
+const StepItem = styled.div<StepItemProps>(({ active }) => {
+	return css`
+		width: ${active ? "48px" : "24px"};
+		height: 6px;
+		border-radius: 3px;
+		background: ${active
+			? "linear-gradient(90deg, #CD8A64, #C4794F);"
+			: "#9f9f9f"};
+	`;
+});
+
+const StyledStepper = styled.div`
+	display: flex;
+	gap: 8px;
+`;
+
+export const Stepper: React.FC<Props> = ({ stepCount, activeStep }) => {
+	return (
+		<StyledStepper>
+			{[...Array(stepCount)].map((_, i) => (
+				<StepItem active={i === activeStep} />
+			))}
+		</StyledStepper>
+	);
+};
